47d91f30cff2deaf82e960f41b733c99292f7bd0
Hassan Ayoub Zafar <60160952+hassan-zafar@users.noreply.github.com>
2021-12-08 04:20:36 +0500

    Update Inbox.test.js

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -2,16 +2,21 @@ const ganache=require('ganache-cli');
 const assert=require('assert');
 const Web3=require('web3');
 const web3 =new Web3(ganache.provider());
+const {interface, bytecode}=require('../compile');
 
 let accounts;
+let inbox;
 beforeEach(async ()=>{
 
    accounts= await web3.eth.getAccounts();
+   inbox=await new web3.eth.Contract(JSON.parse(interface))
+   .deploy({data:bytecode,arguments:['Hi There']})
+   .send({from:accounts[0],gas:'1000000'});
     });
 
 
 describe('Inbox',()=>{
     it('deploys a contract',()=>{
-console.log(accounts);
+assert.ok(inbox.options.address);
     });
 });

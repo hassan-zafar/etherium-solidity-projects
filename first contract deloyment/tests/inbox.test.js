643424dc58284f5b8bf675dd2c19a05e6f73151c
Hassan Ayoub Zafar <60160952+hassan-zafar@users.noreply.github.com>
2021-12-08 02:59:55 +0500

    .

--- first contract deloyment/tests/inbox.test.js
+++ first contract deloyment/tests/inbox.test.js
@@ -2,4 +2,14 @@ const ganache=require('ganache-cli');
 const assert=require('assert');
 const Web3=require('web3');
 const web3 =new Web3(ganache.provider());
-web3.eth.getAccounts
+
+beforeEach(()=>{
+
+    web3.eth.getAccounts().then(fetchedAccounts=>{
+        console.log(fetchedAccounts);
+    });
+});
+
+describe('Indox',()=>{
+    it('deploys a contract',()=>{});
+});

f475a2d22e6d0631b9761619cc2c9db802c19a0b
Hassan Ayoub Zafar <60160952+hassan-zafar@users.noreply.github.com>
2021-12-08 05:10:57 +0500

    Update Inbox.test.js

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -23,4 +23,9 @@ assert.ok(inbox.options.address);
       const message=await inbox.methods.message().call();
       assert.equal(message,'Hi There');
     });
+    it('can change the message',async()=>{
+      await inbox.methods.setMessage("bye").send({from:accounts[0]});
+      const message=await inbox.methods.message().call();
+      assert.equal(message,'bye');
+    });
 });

eac5ae8eb9cb4772abcbc0f05b2a9945097ba171
Hassan Ayoub Zafar <60160952+hassan-zafar@users.noreply.github.com>
2021-12-09 04:28:30 +0500

    Update deploy.js

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -2,6 +2,6 @@ const HDWalletProvider=require('truffle-hdwallet-provider');
 const Web3=require('web3');
 const {interface,bytecode}=require('./compile');
 
-const provider=new HDWalletProvider('trumpet month push switch figure senior vacant engine arctic detect unknown witness',
+const provider=new HDWalletProvider('your code',
                                     'https://rinkeby.infura.io/v3/b1a7c93391264b1788627c9a0c4d087e');
 const web3=new Web3(provider);
